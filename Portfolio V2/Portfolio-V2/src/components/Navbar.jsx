c2a3eeadd594e24392498c1a40af90369874c09b
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-10-24 11:30:46 -0300

    Segundo  Update

--- Portfolio V2/Portfolio-V2/src/components/Navbar.jsx
+++ Portfolio V2/Portfolio-V2/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center p-4 bg-gray-800 text-white">
+    <nav className="fixed top-0 left-0 right-0 z-10 p-4 bg-gray-800 text-white">
       <div className="text-2xl font-bold">Mi Portfolio</div>
       <div className="flex items-center space-x-4">
         <a href="#" className="text-gray-300 hover:text-white">Inicio</a>
